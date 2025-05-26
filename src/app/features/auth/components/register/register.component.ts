a52c587b0cd8c22cd34e4c8bda7385197da2e448
2017PGCACA01 <ankitmondal.nitjsr@gmail.com>
2025-05-26 11:18:53 +0530

    bug(register.component): removed fullname field

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -19,7 +19,6 @@ export class RegisterComponent {
     private router: Router
   ) {
     this.registerForm = this.fb.group({
-      fullName: ["", Validators.required],
       email: ["", [Validators.required, Validators.email]],
       password: ["", [Validators.required, Validators.minLength(8)]],
       role: [UserRole.VIEWER, Validators.required],
